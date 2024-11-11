8cd2241b4673239bf316b1bb35ad86c9fe9f898c
Keerthana V <78976627+Keerthanabalaje@users.noreply.github.com>
2024-11-11 18:19:43 +0530

    Update reducer.js

--- reducer.js
+++ reducer.js
@@ -1,17 +1,20 @@
-import {SET_USERNAME} from'./actions;
-import {SET_PASSWORD} from './actions'
-const initialState={
-    username:"",
-    password:"",
-}
-const loginReducer=(state=initialState,action)=>{
-    switch (action.type){
-        case SET_USERNAME:
-            return{...state,username:action.payload};
-        case SET_PASSWORD:
-            return{...state,password:action.payload};
-        default:
-            return state
-    }
-}
+// src/redux/reducer.js
+import { SET_USERNAME, SET_PASSWORD } from './actions';
+
+const initialState = {
+  username: '',
+  password: '',
+};
+
+const loginReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case SET_USERNAME:
+      return { ...state, username: action.payload };
+    case SET_PASSWORD:
+      return { ...state, password: action.payload };
+    default:
+      return state;
+  }
+};
+
 export default loginReducer;
