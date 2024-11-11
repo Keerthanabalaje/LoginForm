c28d9df41f5544f9a83fad817e89e63996650435
Keerthana V <78976627+Keerthanabalaje@users.noreply.github.com>
2024-11-11 18:18:03 +0530

    Update Login.js

--- Login.js
+++ Login.js
@@ -1,38 +1,50 @@
-import React from 'react'
-import {useDispatch,useSelector} from 'react-redux'
-import {setUsername } from './redux/actions';
-import {setPassword } from './redux/actions';
-const Login=()=>{
-    const dispatch=useDispatch()
-    const {username,password}=useSelector((state)=>state);
-    const handleUsername=(e)=>{
-        dispatch(setUsername(e.target.value);)
-    };
-    const handlePassword=(e)=>{
-        dispatch(setPassword(e.target.value);)
-    };
-    const handleSubmit=(e)=>{
-        e.preventDefault()
-        console.log("Attempted login",{username})
-    }
-    return(
-        <div>
-            <h1>Login</h1>
-            <form onSubmit={handleSubmit}>
-                <div>
-                    <label>UserName : </label>
-                    <input type="text"value={username}
-                    onChange={handleUsername} />
-                </div>
-                <div>
-                <label>Password : </label>
-                    <input type="password"value={password}
-                    onChange={handlePassword}/>
+// src/components/Login.js
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { setUsername, setPassword } from '../redux/actions';
 
-                </div>
-                <button type="submit">Submit</button>
-                </form>
-            </div>
-    )
-}
+const Login = () => {
+  const dispatch = useDispatch();
+  const { username, password } = useSelector((state) => state);
+
+  const handleUsernameChange = (e) => {
+    dispatch(setUsername(e.target.value));
+  };
+
+  const handlePasswordChange = (e) => {
+    dispatch(setPassword(e.target.value));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // You can handle form submission here
+    console.log('Login Attempt:', { username, password });
+  };
+
+  return (
+    <div>
+      <h2>Login</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>Username:</label>
+          <input
+            type="text"
+            value={username}
+            onChange={handleUsernameChange}
+          />
+        </div>
+        <div>
+          <label>Password:</label>
+          <input
+            type="password"
+            value={password}
+            onChange={handlePasswordChange}
+          />
+        </div>
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+};
+
 export default Login;
